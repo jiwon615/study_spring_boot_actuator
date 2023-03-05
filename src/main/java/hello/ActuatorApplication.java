d7a7ac86f8f6ee06d9360d39cedde0d9f94beecf
jiwon615 <seg615@naver.com>
2023-03-05 15:57:08 +0900

    feat: 앤드포인트 httpexchanges 사용

--- src/main/java/hello/ActuatorApplication.java
+++ src/main/java/hello/ActuatorApplication.java
@@ -1,13 +1,22 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class ActuatorApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(ActuatorApplication.class, args);
+    }
+
+    // httpexchanges 앤드포인트 사용위해 추가
+    @Bean
+    public InMemoryHttpExchangeRepository httpExchangeRepository() {
+        // 이 구현체는 최대 100갸의 HTTP 요청을 제공함. 최대 요청이 넘어가면 과거 요청 삭제함 ( setCapacity()로 최대 요청수 변경 가능)
+        return new InMemoryHttpExchangeRepository();
     }
 
 }
